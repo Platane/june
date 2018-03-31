9686cc0db1e14b90fb93d728c044d697cb9e5c03
platane <me@platane.me>
2018-03-31 12:31:41 +0100

    :rocket: read from API

--- src/component/_container/withShow.js
+++ src/component/_container/withShow.js
@@ -1,11 +1,33 @@
 import { h, Component } from 'preact'
 import { shows } from '~/__fixtures__'
+import { getShowWithCastById } from '~/service/tvmaze'
 
 export default C =>
   class WithShow extends Component {
-    render() {
-      return (
-        <C {...this.props} show={shows.find(x => x.id === this.props.showId)} />
+    state = { pending: null, show: null }
+
+    update = async () => {
+      if (
+        this.show &&
+        this.show.id == this.props.showId &&
+        this.props.showId == this.state.pending
       )
+        return
+
+      this.setState({ pending: this.props.showId })
+
+      const show = await getShowWithCastById(this.props.showId)
+
+      if (this.props.showId != this.state.pending) return
+
+      this.setState({ pending: null, show })
+    }
+
+    componentDidMount() {
+      this.update()
+    }
+
+    render() {
+      return <C {...this.props} {...this.state} />
     }
   }
--- src/component/_container/withShows.js
+++ src/component/_container/withShows.js
@@ -1,11 +1,28 @@
 import { h, Component } from 'preact'
 import { shows } from '~/__fixtures__'
+import { createGetLastShows } from '~/service/tvmaze'
 
 export default C =>
   class WithShows extends Component {
-    state = { shows }
+    state = { pending: null, shows: [] }
+
+    getLastShows = createGetLastShows()
+
+    loadMore = async () => {
+      if (this.state.pending) return
+
+      this.setState({ pending: true })
+
+      const shows = await this.getLastShows()
+
+      this.setState({ pending: null, shows: [...this.state.shows, ...shows] })
+    }
+
+    componentDidMount() {
+      this.loadMore()
+    }
 
     render() {
-      return <C {...this.props} {...this.state} />
+      return <C {...this.props} {...this.state} loadMore={this.loadMore} />
     }
   }
