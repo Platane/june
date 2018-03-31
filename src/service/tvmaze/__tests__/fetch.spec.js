c54b7659f24eb09bd479de42f1459d9d23abb8bb
platane <me@platane.me>
2018-03-31 11:59:46 +0100

    :rocket: getLastShows

--- src/service/tvmaze/__tests__/fetch.spec.js
+++ src/service/tvmaze/__tests__/fetch.spec.js
@@ -1,5 +1,5 @@
 import test from 'tape'
-import { getShowsByName, getCastForShow } from '../index'
+import { getShowsByName, getCastForShow, createGetLastShows } from '../index'
 import type { Show, CastItem } from 'type'
 
 const flatten = arr => [].concat(...arr)
@@ -29,6 +29,30 @@ test('getCastForShow', async t => {
   t.assert(cast.length, 'result should not be empty')
 
   t.assert(cast.every(s => CastItem.assert(s)), 'should return cast object')
+
+  t.end()
+})
+
+test('createGetLastShows', async t => {
+  const getLastShows = createGetLastShows()
+
+  const shows = [
+    ...(await getLastShows()),
+    ...(await getLastShows()),
+    ...(await getLastShows()),
+    ...(await getLastShows()),
+  ]
+
+  t.pass('request should not fail')
+
+  t.assert(shows.length, 'result should not be empty')
+
+  t.assert(shows.every(s => Show.assert(s)), 'should return shows object')
+
+  t.assert(
+    shows.every((s, i) => shows.findIndex(u => s.id == u.id) === i),
+    'should list a show only once'
+  )
 
   t.end()
 })
--- src/service/tvmaze/index.js
+++ src/service/tvmaze/index.js
@@ -1,13 +1,54 @@
 import { parseShow, parseCast } from './parse'
+import type { Show, CastItem } from 'type'
 
 const endpoint = 'https://api.tvmaze.com'
 
-export const getShowsByName = async (pattern: string) =>
+export const getShowsByName = (pattern: string): Promise<Show[]> =>
   fetch(`${endpoint}/search/shows?q=${pattern}`)
     .then(res => res.json())
     .then(res => res.map(x => parseShow(x.show)))
 
-export const getCastForShow = async (showId: string) =>
+export const getCastForShow = (showId: string): Promise<CastItem[]> =>
   fetch(`${endpoint}/shows/${showId}/cast`)
     .then(res => res.json())
     .then(res => res.map(parseCast))
+
+export const createGetLastShows = () => {
+  const alreadyRead = {}
+
+  let cursor = new Date().toISOString().slice(0, 10)
+
+  const getShowAtDate = (date: string): Promise<Show[]> =>
+    fetch(`${endpoint}/schedule?country=US&date=${date}`)
+      .then(res => res.json())
+      .then(res => res.map(x => parseShow(x.show)))
+
+  const getNextCursor = date => {
+    const a = new Date(date)
+    a.setDate(a.getDate() - 1)
+
+    return a.toISOString().slice(0, 10)
+  }
+
+  return async (): Promise<Show[]> => {
+    const newOnes = []
+
+    let k = 5
+
+    while (newOnes.length < 10 && k-- > 0) {
+      const shows = await getShowAtDate(cursor)
+
+      cursor = getNextCursor(cursor)
+
+      shows.forEach(s => {
+        if (alreadyRead[s.id]) return
+
+        alreadyRead[s.id] = true
+
+        newOnes.push(s)
+      })
+    }
+
+    return newOnes
+  }
+}
